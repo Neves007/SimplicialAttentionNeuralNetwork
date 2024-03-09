438d6ce994a463af662ff256a165c8ea20d15f72
Neves007 <2214519556@qq.com>
2024-03-09 14:42:48 +0800

    2023-3-9

--- mydynalearn/analyze/analyze_manager.py
+++ mydynalearn/analyze/analyze_manager.py
@@ -4,10 +4,15 @@ from mydynalearn.config import Config
 
 class AnalyzeManager():
     def __init__(self,train_experiment_manager):
+        '''
+        分析epoch
+
+        :param train_experiment_manager:
+        '''
         config_analyze = Config.get_config_analyze()
         self.config = config_analyze['default']
         self.train_experiment_manager = train_experiment_manager
-        self.r_value_analyzer = RValueAnalyzer(self.config)
+        self.epoch_analyzer = epochAnalyzer(self.config)
 
     def _get_model_executor_generator(self):
         exp_generator = self.train_experiment_manager.get_exp_generator()  # 所有的实验对象
@@ -22,7 +27,7 @@ class AnalyzeManager():
     def get_analyze_result_generator_for_best_epoch(self):
         model_executor_generator = self._get_model_executor_generator()
         for model_executor in model_executor_generator:
-            best_epoch_index = self.r_value_analyzer.get_best_epoch_index(**model_executor.global_info)
+            best_epoch_index = self.epoch_analyzer.get_best_epoch_index(**model_executor.global_info)
             EPOCHS = model_executor.EPOCHS
             best_epoch = list(range(EPOCHS))[best_epoch_index]
             best_epoch_analyze_result = model_executor.run(best_epoch)
@@ -37,6 +42,9 @@ class AnalyzeManager():
                 yield analyze_result
 
     def buid_anayze_result(self):
+        '''
+        将所有实验的数据集引入到自己的模型中，输出analyze_result
+        '''
         model_executor_generator = self._get_model_executor_generator()
         for model_executor in model_executor_generator:
             EPOCHS = model_executor.EPOCHS
@@ -45,29 +53,26 @@ class AnalyzeManager():
                 if need_to_run:
                     model_executor.run(model_exp_epoch_index)
 
-    def add_r_value_for_all_epochs(self):
-        '''
-        将所有实验的数据集引入到自己的模型中，输出analyze_result
-        '''
-        r_value_dataframe_is_exist = os.path.exists(self.r_value_analyzer.r_value_dataframe_file_path)
-        if not r_value_dataframe_is_exist:
+    def analyze_all_epochs(self):
+        all_epoch_dataframe_is_exist = os.path.exists(self.epoch_analyzer.all_epoch_dataframe_file_path)
+        if not all_epoch_dataframe_is_exist:
             analyze_result_generator = self.get_analyze_result_generator_for_all_epochs()
             for analyze_result in analyze_result_generator:
-                    self.r_value_analyzer.add_r_value(analyze_result)
-            self.r_value_analyzer.save_r_value_dataframe()
+                    self.epoch_analyzer.add_epoch_result(analyze_result)
+            self.epoch_analyzer.save_all_epoch_dataframe()
         else:
-            self.r_value_analyzer.load_r_value_dataframe()
+            self.epoch_analyzer.load_all_epoch_dataframe()
 
 
-    def analyze_stable_r_value(self):
+    def analyze_best_epoch(self):
         '''分析R值的稳定点
 
         io:  stable_r_value_dataframe.csv
         ''' 
-        if not os.path.exists(self.r_value_analyzer.stable_r_value_dataframe_file_path):
-            self.r_value_analyzer.analyze_stable_r_value(self)
+        if not os.path.exists(self.epoch_analyzer.best_epoch_dataframe_file_path):
+            self.epoch_analyzer.analyze_best_epoch()
         else:
-            self.r_value_analyzer.load_stable_r_value_dataframe()
+            self.epoch_analyzer.load_best_epoch_dataframe()
     
 
 
@@ -81,9 +86,9 @@ class AnalyzeManager():
         print("*" * 10 + " ANALYZE TRAINED MODEL " + "*" * 10)
         print("buid anayze result")
         self.buid_anayze_result()
-        print("add r value for all epochs")
-        self.add_r_value_for_all_epochs()
-        print("analyze stable r value")
-        self.analyze_stable_r_value()
+        print("analyze all epochs")
+        self.analyze_all_epochs()
+        print("analyze best epoch")
+        self.analyze_best_epoch()
 
 
